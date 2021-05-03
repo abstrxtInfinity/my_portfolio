cd8b56417b500d0a1f8fd417bf512f20f7f0c5d2
abstrxtInfinity <anantverma2001@gmail.com>
2021-05-03 21:24:22 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+//* Navbar toggle
+function toggleMenu() {
+    const menuToggle = document.querySelector('.icon');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+
+//!--------------------------------------------------------
+
+//* Theme toggle
+// var checkbox = document.querySelector('input[name=theme]');
+// checkbox.addEventListener('change', function () {
+//     if (this.checked) {
+//         trans()
+//         document.documentElement.setAttribute('data-theme', 'dark')
+//     } else {
+//         trans()
+//         document.documentElement.setAttribute('data-theme', 'light')
+//     }
+
+// })
+// let trans = () => {
+//     document.documentElement.classList.add('transition');
+//     window.setTimeout(() => {
+//         document.documentElement.classList.remove('transition')
+//     }, 1000)
+// }
+
+//!--------------------------------------------------------
+
+//* FORM Behavior
+
+// var form = document.getElementById("my-form");
+// function error() { alert("Oops! Something went wrong"); };
+// function success() { alert("Message Delivered"); };
+
+// async function handleSubmit(event) {
+//     event.preventDefault();
+//     var data = new FormData(event.target);
+//     fetch(event.target.action, {
+//         method: form.method,
+//         body: data,
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }).then(response => {
+//         // console.log(response);
+//         form.reset();
+//         setTimeout(success, 3000);
+//     }).catch(error => {
+//         console.log(error);
+//         setTimeout(error, 3000);
+//     });
+// }
+// form.addEventListener("submit", handleSubmit);
+
+//!--------------------------------------------------------
