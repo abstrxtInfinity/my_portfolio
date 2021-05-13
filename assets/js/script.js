de364f7dd35cbfb826bde72115ef6fb5c288db46
abstrxtInfinity <anantverma2001@gmail.com>
2021-05-13 23:13:08 +0530

    dark mode completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,6 +168,7 @@ const scrollToTopButton = document.getElementById('js-top');
 // Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
 const scrollFunc = () => {
     // Get the current scroll value
+
     let y = window.scrollY;
 
     // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
@@ -176,6 +177,8 @@ const scrollFunc = () => {
     } else {
         scrollToTopButton.className = "top-link hide";
     }
+
+
 };
 
 window.addEventListener("scroll", scrollFunc);
