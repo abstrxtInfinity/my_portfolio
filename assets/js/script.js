ade0601cb7be66ad7099e0d9a6c181fd923c7cc2
abstrxtInfinity <anantverma2001@gmail.com>
2021-05-13 21:10:47 +0530

    theme-toggler icons and style added

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,7 @@ for (var i = 0; i < 28; i++) {
     effect.innerHTML += '<div class ="circle"></div>';
 }
 
+
 function bodyScrollingToggle() {
     document.body.classList.toggle("stop-scrolling");
 }
@@ -159,58 +160,95 @@ function bodyScrollingToggle() {
 
 
 
+// !---------------------------Back to Top------------------------
+
+// Set a variable for our button element.
+const scrollToTopButton = document.getElementById('js-top');
+
+// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+const scrollFunc = () => {
+    // Get the current scroll value
+    let y = window.scrollY;
+
+    // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+    if (y > 300) {
+        scrollToTopButton.className = "top-link show";
+    } else {
+        scrollToTopButton.className = "top-link hide";
+    }
+};
+
+window.addEventListener("scroll", scrollFunc);
 
 
+//!--------------------------------------------------------
 
 
 
 //!--------------------------------------------------------
 
-//* Theme toggle
-// var checkbox = document.querySelector('input[name=theme]');
-// checkbox.addEventListener('change', function () {
-//     if (this.checked) {
-//         trans()
-//         document.documentElement.setAttribute('data-theme', 'dark')
-//     } else {
-//         trans()
-//         document.documentElement.setAttribute('data-theme', 'light')
-//     }
+// * Side theme box toggle
+var boxToggleIcon = document.getElementById('box-toggle-icon');
+var box = boxToggleIcon.parentElement.querySelector('.toggle-box');
 
-// })
-// let trans = () => {
-//     document.documentElement.classList.add('transition');
-//     window.setTimeout(() => {
-//         document.documentElement.classList.remove('transition')
-//     }, 1000)
-// }
+boxToggleIcon.addEventListener("click", () => {
+    box.classList.toggle('open');
+
+});
+
+function scrollClose() {
+    let y = window.scrollY;
+    if (y > 0 && box.classList.contains("open")) {
+        box.classList.remove('open');
+    }
+};
+window.addEventListener("scroll", scrollClose);
+
+//* Theme toggle
+var checkbox = document.querySelector('input[name=theme]');
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+
+})
+let trans = () => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000)
+}
 
 //!--------------------------------------------------------
 
 //* FORM Behavior
 
-// var form = document.getElementById("my-form");
-// function error() { alert("Oops! Something went wrong"); };
-// function success() { alert("Message Delivered"); };
+var form = document.getElementById("my-form");
+function error() { alert("Oops! Something went wrong"); };
+function success() { alert("Message Delivered"); };
 
-// async function handleSubmit(event) {
-//     event.preventDefault();
-//     var data = new FormData(event.target);
-//     fetch(event.target.action, {
-//         method: form.method,
-//         body: data,
-//         headers: {
-//             'Accept': 'application/json'
-//         }
-//     }).then(response => {
-//         // console.log(response);
-//         form.reset();
-//         setTimeout(success, 3000);
-//     }).catch(error => {
-//         console.log(error);
-//         setTimeout(error, 3000);
-//     });
-// }
-// form.addEventListener("submit", handleSubmit);
+async function handleSubmit(event) {
+    event.preventDefault();
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        // console.log(response);
+        form.reset();
+        setTimeout(success, 3000);
+    }).catch(error => {
+        console.log(error);
+        setTimeout(error, 3000);
+    });
+}
+form.addEventListener("submit", handleSubmit);
 
 //!--------------------------------------------------------
